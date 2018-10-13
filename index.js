c789ff649fc190d76e480bc090b006b965f7884e
Nat Reid <nathanielreid@gmail.com>
2018-10-13 01:09:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ function kittens(){
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  return [...kittens,name]
+}
+function prependKitten(name){
+  return [name,...kittens]
+}
 // Add your functions and code here
