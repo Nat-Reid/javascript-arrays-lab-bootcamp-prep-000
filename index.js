570af60dbac93033271ba0e01341b068ce98f0e9
Nat Reid <nathanielreid@gmail.com>
2018-10-13 01:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function prependKitten(name){
   return [name,...kittens]
 }
 function removeLastKitten(){
-  return kittens.pop()
+  return kittens.slice(0,kittens.length-1)
 }
 function removeFirstKitten(){
-  return kittens.shift()
+  return kittens.slice(1)
 }
 // Add your functions and code here
